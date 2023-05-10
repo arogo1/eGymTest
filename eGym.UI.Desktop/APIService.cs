c31e53bbc521599e7c3006cef0314a5bb6d6a07f
Arnes Rogo <Arnes.Rogo@poly.com>
2023-05-10 09:34:33 +0200

    Implement WIN for Employee

---
+++ eGym.UI.Desktop/APIService.cs
@@ -0,0 +1,108 @@
+﻿using eGym.BLL.Models;
+using Flurl.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Text;
+
+namespace eGym.UI.Desktop
+{
+    public class APIService
+    {
+        private string _resource = null;
+        public string _endpoint = "https://localhost:7220/";
+
+        public static string Username = null;
+        public static string Password = null;
+        public static EmployeeDTO currentUser;
+
+        public APIService(string resource)
+        {
+            _resource = resource;
+        } 
+
+        public async Task<T> Login<T>(object request, string path = "")
+        {
+            try
+            {
+                var query = "";
+                if (request != null)
+                {
+                    query = await request.ToQueryString();
+                }
+
+                var result = await $"{_endpoint}{_resource}{path}?{query}".PostAsync().ReceiveJson<T>();
+                return result;
+            }
+            catch (FlurlHttpException ex)
+            {
+                var errors = await ex.GetResponseJsonAsync<Dictionary<string, string[]>>();
+
+                var stringBuilder = new StringBuilder();
+                foreach (var error in errors)
+                {
+                    stringBuilder.AppendLine($"{error.Key}, ${string.Join(",", error.Value)}");
+                }
+
+                MessageBox.Show(stringBuilder.ToString(), "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return default(T);
+            }
+        }
+
+        public async Task<T> Post<T>(object request)
+        {
+            try
+            {
+                var result = await $"{_endpoint}{_resource}".WithBasicAuth(Username, Password).PostJsonAsync(request).ReceiveJson<T>();
+                return result;
+            }
+            catch (FlurlHttpException ex)
+            {
+                var errors = await ex.GetResponseJsonAsync<Dictionary<string, string[]>>();
+
+                var stringBuilder = new StringBuilder();
+                foreach (var error in errors)
+                {
+                    stringBuilder.AppendLine($"{error.Key}, ${string.Join(",", error.Value)}");
+                }
+
+                MessageBox.Show(stringBuilder.ToString(), "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return default(T);
+            }
+        }
+
+        public async Task<T> Get<T>(object search = null, string path = "")
+        {
+            var query = "";
+            if (search != null)
+            {
+                query = await search.ToQueryString();
+            }
+
+            var list = await $"{_endpoint}{_resource}{path}?{query}".WithBasicAuth(Username, Password).GetJsonAsync<T>();
+
+            return list;
+        }
+
+        public async Task<T> GetById<T>(object id)
+        {
+            var result = await $"{_endpoint}{_resource}/{id}".WithBasicAuth(Username, Password).GetJsonAsync<T>();
+
+            return result;
+        }
+
+        public async Task<T> Put<T>(object id, object request)
+        {
+            var result = await $"{_endpoint}{_resource}/{id}".WithBasicAuth(Username, Password).PutJsonAsync(request).ReceiveJson<T>();
+
+            return result;
+        }
+
+        public async Task Delete(object id)
+        {
+            var query =  await id.ToQueryString();
+
+            var result = await $"{_endpoint}{_resource}{query}".WithBasicAuth(Username, Password).DeleteAsync();
+        }
+    }
+}
---
+++ eGym.UI.Desktop/frmEmployee.cs
@@ -0,0 +1,74 @@
+﻿using eGym.BLL.Models;
+
+namespace eGym.UI.Desktop
+{
+    public partial class frmEmployee : Form
+    {
+        private readonly APIService _service = new APIService("Employee");
+        private EmployeeDTO selectedEmployee = new EmployeeDTO();
+
+        public frmEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private async void frmEmployee_Load(object sender, EventArgs e)
+        {
+            dgvEmployee.DataSource = await _service.Get<List<EmployeeDTO>>(null, "/getAll");
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            dgvEmployee.DataSource = await _service.Get<List<EmployeeDTO>>(new { text = txtSearch.Text}, "/search");
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            await _service.Delete(selectedEmployee.EmployeeId);
+        }
+
+        private async void btnSave_Click(object sender, EventArgs e)
+        {
+            await _service.Put<EmployeeDTO>(selectedEmployee.EmployeeId, null);
+        }
+
+        private void dgvKorisinici_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var item = dgvEmployee.SelectedRows[0].DataBoundItem as EmployeeDTO;
+
+            selectedEmployee = item;
+        }
+
+        private void btnCreateEmployee_Click(object sender, EventArgs e)
+        {
+            frmCreateEmployee frm = new frmCreateEmployee();
+
+            frm.Show();
+
+            //this.Close();
+        }
+
+        private void dgvEmployee_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            selectedEmployee = dgvEmployee.Rows[index].DataBoundItem as EmployeeDTO;
+
+            txtId.Text = selectedEmployee.EmployeeId.ToString();
+            txtName.Text = selectedEmployee.FirstName;
+            txtLastName.Text = selectedEmployee.LastName;
+            txtPassword.Text = selectedEmployee.Password;
+            txtUsername.Text = selectedEmployee.Username;
+            dateTimePicker1.Value = selectedEmployee.BirthDate;
+            comboBox1.SelectedIndex = (int)selectedEmployee.Role;
+
+            if(selectedEmployee.Gender == BLL.Models.Enums.Gender.Female)
+            {
+                rbZensko.Checked = true;
+            }
+            else
+            {
+                rbMale.Checked = true;
+            }
+        }
+    }
+}
