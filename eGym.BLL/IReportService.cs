677c2ddd0947be61267ca27e11ef19716ce49b4b
Amra Hindić <amra.hindic@edu.fit.ba>
2023-07-04 23:32:34 +0200

    Your commit message

--- eGym.BLL/IReportService.cs
+++ eGym.BLL/IReportService.cs
@@ -5,6 +5,7 @@ namespace eGym.BLL;
 
 public interface IReportService
 {
+
     public Task<byte[]> GetUserReport();
     public Task<byte[]> GetFinanceReport();
     public Task<byte[]> GetEmployeeReport();
